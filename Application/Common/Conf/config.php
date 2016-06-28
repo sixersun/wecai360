7696e0d5b7c8dd99c22d4494b9ced338cbbe9fd1
夏韧旭 <xiarenxu@nears.cn>
2016-06-29 02:16:34 +0800

    修改数据库
    修改数据库

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -8,8 +8,8 @@ return array(
 	'DB_RW_SEPARATE'	=> true,
 	'DB_HOST'			=> '127.0.0.1', //服务器地址
 	'DB_NAME'			=> 'wecai360', //数据库名
-	'DB_USER'			=> 'root',			//用户名
-	'DB_PWD'			=> '',		//密码
+	'DB_USER'			=> 'wecai360',			//用户名
+	'DB_PWD'			=> 'aNMYHWQSWTnr2QYD',		//密码
     // 设置默认模块
     'DEFAULT_MODULE' => 'Home',
     
