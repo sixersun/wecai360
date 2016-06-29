e2ccdd83efa95532e1c640f6934fd642b0536ea0
夏韧旭 <xiarenxu@nears.cn>
2016-06-30 01:35:33 +0800

    修改导出

--- Application/Home/Controller/ShoppingController.class.php
+++ Application/Home/Controller/ShoppingController.class.php
@@ -2,46 +2,47 @@
 namespace Home\Controller;
 use Think\Controller;
 class ShoppingController extends Controller {
-	public function __construct(){
-		parent::__construct();
-		islogin();
-		$this->user=session('user');
-	}
+    public function __construct(){
+        parent::__construct();
+        islogin();
+        $this->user=session('user');
+    }
     public function index(){
-    	$user=session('user');
-    	$user_id=$user['id'];
-    	$list=D('Shopping')->getshopping($user_id);
-    	$this->assign('list',$list);
-    	$this->display('./shopping');
+        $user=session('user');
+        $user_id=$user['id'];
+        $list=D('Shopping')->getshopping($user_id);
+        $this->assign('list',$list);
+        $this->display('./shopping');
     }
     public function in(){
-    	if(!$_POST) return false;
-    	$data=I('post.data');
-    	$arr=array(
-    		'user_id'=>$this->user['id'],
-    		'pro_id'=>$data['id'],
-    		'code'=>$data['code'],
-    		'proname'=>$data['proname'],
-    		'pro_num'=>$data['num']?$data['num']:1,
+        if(!$_POST) return false;
+        $data=I('post.data');
+        $arr=array(
+            'user_id'=>$this->user['id'],
+            'pro_id'=>$data['id'],
+            'code'=>$data['code'],
+            'proname'=>$data['proname'],
+            'pro_num'=>$data['num']?$data['num']:1,
             'supplier'=>$data['supplier'],
-    		'price'=>$data['price'],
+            'price'=>$data['price'],
             'model'=>$data['model'],
             'note'=>$data['note'],
             'img'=>$data['img'],
-    		'status'=>1,
-    		);
-    	if(!D('shopping')->saveshooping($arr)){
-    		ajax(-1,'添加到购物车失败',null);
-    	}
-    	ajax(1,'添加到购物车',null);
+            'status'=>1,
+            );
+        if(!D('shopping')->saveshooping($arr)){
+            ajax(-1,'添加到购物车失败',null);
+        }
+        ajax(1,'添加到购物车',null);
     }
     public function export_excel(){
-        $data=I('post.data');
 
+        $data=trim($_POST['data']);
+        $data=json_decode($data,true);
         foreach ($data as $k => $v) {
             $arr[]=array($k+1,$v['img'],$v['proname'],$v['model'],$v['code'],$v['note'],$v['price'],$v['pro_num']);
         }
-        $this->push($data);
+        $this->push($arr);
     }
     public function push($data){
         import('@.Org.excel.PHPExcel');
