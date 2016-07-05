62fb95a5256ab03936b3b3b82c83beef4e48bc12
夏韧旭 <xiarenxu@nears.cn>
2016-07-06 01:41:39 +0800

    pdf

--- Application/Home/Controller/ShoppingController.class.php
+++ Application/Home/Controller/ShoppingController.class.php
@@ -108,21 +108,78 @@ class ShoppingController extends Controller {
         header("Content-Transfer-Encoding:binary");
         $write->save('php://output');
     }
-    private function out_excel($PHPExcel, $filename) {
-        ob_clean();
-        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        if(strpos($_SERVER['HTTP_USER_AGENT'],"MSIE")) {
-            //如果是ie存为的名字要urlencode
-            header('Content-Disposition: attachment; filename="'.urlencode($filename).'"');
-        } else {
-            header('Content-Disposition: attachment; filename="'.$filename.'"');//存为的名字
+    public function outpdf(){
+        $data=trim($_POST['html']);
+        $data=json_decode($data,true);
+        $html='<table><tr><th width="50">序号</th><th align="center" width="100">图片</th><th align="center">产品名称</th><th>产品型号</th><th>编号</th><th width="120">备注</th><th>价格</th><th>数量</th></tr>';
+        $str='';
+        foreach ($data as $k => $v) {
+            if(!empty($data[$k])){
+                $str='<tr><td>'.($k+1).'</td><td width="100"><img src="'.$v['img'].'"></td><td align="center">'.$v['proname'].'</td><td>'.$v['model'].'</td><td>'.$v['code'].'</td><td>'.$v['note'].'</td><td>'.$v['price'].'</td><td>'.$v['pro_num'].'</td></tr>';
+            }
+                    $html=$html.$str; 
         }
-        header('Content-Transfer-Encoding: binary');//可删否
-        header('Cache-Control: max-age=0');
-        ob_clean();//关键
-        flush();//关键
-        $Writer = \PHPExcel_IOFactory::createWriter($PHPExcel, 'Excel5');
-        $Writer->save('php://output');
 
+        $html=$html.'</table>';
+        import('@.Org.tcpdf.tcpdf');
+        $pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        // set document information
+        $pdf->SetCreator(PDF_CREATOR);
+        $pdf->SetAuthor('Rootxia');
+        $pdf->SetTitle('adb');
+        $pdf->SetSubject('aaa');
+        $pdf->SetKeywords('bbbb');
+        // set default header data
+        $pdf->SetHeaderData('wecailogo.png', '30', '', '', array(0,64,255), array(0,64,128));
+        $pdf->setFooterData(array(0,64,0), array(0,64,128));
+
+        // set header and footer fonts
+        $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+        $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+        // set default monospaced font
+        $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+        // set margins
+        $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+        $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+
+        // set auto page breaks
+        $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+
+        // set image scale factor
+        $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+        // set some language-dependent strings (optional)
+        if (@file_exists(APP_PATH.'Home/Org/tcpdf/examples/lang/eng.php')) {
+            require_once(APP_PATH.'Home/Org/tcpdf/examples/lang/eng.php');
+            $pdf->setLanguageArray($l);
+        }
+
+        // ---------------------------------------------------------
+
+        // set default font subsetting mode
+        $pdf->setFontSubsetting(true);
+
+        // Set font
+        // dejavusans is a UTF-8 Unicode font, if you only need to
+        // print standard ASCII chars, you can use core fonts like
+        // helvetica or times to reduce file size.
+        $pdf->SetFont('stsongstdlight', '', 12);
+
+        // Add a page
+        // This method has several options, check the source code documentation for more information.
+        $pdf->AddPage();
+
+        // set text shadow effect
+        $pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+
+        // Set some content to print
+        $inhtml='<h1>测试</h1>';
+//         $html = <<<EOD
+//         <>
+// EOD;
+        $pdf->writeHTML($html);
+        $pdf->Output('example_001.pdf', 'I');
     }
 }
